d3b6086d4e71b75c841e9f6fc8f4d611d9be960f
jinny <ertmiu96@gmail.com>
2023-06-16 11:23:57 +0900

    todolist form 내용추가

--- src/js/Form.jsx
+++ src/js/Form.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "../css/main.css";
 //입력 양식 전부를 감싸는 것
-
-function Form({ todos }) {
+let number = 0;
+function Form({ todos, setTodos }) {
   const initialState = {
     id: 0,
     title: "",
@@ -15,33 +15,49 @@ function Form({ todos }) {
   const [todo, setTodo] = useState(initialState);
   function onChangeHandler(event) {
     const { name, value } = event.target;
-    setTodo({ ...todo, [name]: value });
+    setTodo({
+      ...todo,
+      [name]: value,
+    });
   }
-  return (
-    <div className="add-form">
-      y
-      <div className="input-group">
-        <label className="form-label">제목</label>
-        <input
-          type="text"
-          name="title"
-          value={todo.title}
-          className="add-input input-body"
-          onChange={onChangeHandler}
-        />
 
-        <label className="form-label">내용</label>
-        <input
-          type="text"
-          name="body"
-          value={todo.body}
-          className="add-input"
-          onChange={onChangeHandler}
-        />
+  //trim 공백값 지워주기 조건을 걸고 사용
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (todo.title.trim() === "" || todo.body.trim() === "") return;
+    setTodos([...todos, { ...todo, id: number }]);
+    number++;
+  };
+
+  //return 중지 아래의 코드가 실행이 안됨
+  //... 기존의 데이터를 전부 넣는다.
+  //consloe.log로 확인하기
+  // setTodo(initialState); 없을때랑 있을 때 비교해봤는데 아무 문제 없어보여서 일단은 뺌
+  return (
+    <form onSubmit={onSubmitHandler} className="add-form">
+      <div className="add-form">
+        <div className="input-group">
+          <label className="form-label">제목</label>
+          <input
+            type="text"
+            name="title"
+            value={todo.title}
+            className="add-input input-body"
+            onChange={onChangeHandler}
+          />
+
+          <label className="form-label">내용</label>
+          <input
+            type="text"
+            name="body"
+            value={todo.body}
+            className="add-input"
+            onChange={onChangeHandler}
+          />
+        </div>
+        <button className="add-button">추가하기</button>
       </div>
-      <button className="add-button">추가하기</button>
-      추가하기
-    </div>
+    </form>
   );
 }
 
