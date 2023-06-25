e874ef7a60867990cafa92008f097581fa21b6e9
jinny0526 <ertmiu96@gmail.com>
2023-06-25 17:01:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import { useSelector } from "react-redux";
-import "./App.css";
-import Router from "./shared/Router";
 
-function App() {
-  const todos = useSelector(function (state) {
-    return state.todos;
-  });
-  return <div>Reduc TodoList</div>;
-}
-//useSelector내부는 인자로 콜백함수를 넣어야됨
-export default App;
